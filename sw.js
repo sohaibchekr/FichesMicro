d95744ed96fca89b40c3b104d6e1db64ce795280
sohaibchekr <sohaibchekri@gmail.com>
2026-07-24 02:09:47 +0100

    Update service worker to version 4.6.0

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// MICROBIO v4.5.0 - Service Worker avec Cache-First + Offline complet
-const CACHE_NAME = 'microbio-cache-v4.5.0';
+// MICROBIO v4.6.0 - Service Worker avec Cache-First + Offline complet
+const CACHE_NAME = 'microbio-cache-v4.6.0';
 const ASSETS = [
   './',
   './index.html',
